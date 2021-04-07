27a7cec469a756dac8d38133743d6204786df582
Nate Steil <nathanrsteil@gmail.com>
2021-04-07 15:14:51 -0500

    partial progress on rocket class

---
+++ studio/Rocket.ts
@@ -0,0 +1,22 @@
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+import { Payload } from './Payload';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo = []
+    astronauts: Astronaut = []
+    constructor(name:string,totalCapacityKg:number){
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass( items: Payload[] ): number {
+
+    }
+
+    currentMassKg():number {
+        return 
+    }
+}
